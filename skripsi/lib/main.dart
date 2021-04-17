1d8974e2b69939942fcc4f74f1d7731fa6c66f8d
Mohamad Rial Ramli <71802673+rizalramli@users.noreply.github.com>
2021-04-17 12:40:01 +0700

    Login & Register Basic
    Belum edit tampilan

--- skripsi/lib/main.dart
+++ skripsi/lib/main.dart
@@ -1,113 +1,256 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Skripsi'),
+      ),
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: MyApp(),
+      ),
+    ),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool signin = true;
+
+  TextEditingController namectrl, emailctrl, passctrl;
+
+  bool processing = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    namectrl = new TextEditingController();
+    emailctrl = new TextEditingController();
+    passctrl = new TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return Container(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            Icon(
+              Icons.account_circle,
+              size: 200,
+              color: Colors.blue,
+            ),
+            boxUi(),
+          ],
+        ));
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  void changeState() {
+    if (signin) {
+      setState(() {
+        signin = false;
+      });
+    } else
+      setState(() {
+        signin = true;
+      });
+  }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void registerUser() async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      processing = true;
+    });
+    var url = "http://192.168.43.184/skripsi/api/register_api";
+    var data = {
+      "email": emailctrl.text,
+      "nama_user": namectrl.text,
+      "password": passctrl.text,
+    };
+
+    var res = await http.post(url, body: data);
+    var response = json.decode(res.body);
+    if (response['response_message'] == "Email sudah digunakan") {
+      Fluttertoast.showToast(
+          msg: "Email sudah digunakan, Silahkan Login",
+          toastLength: Toast.LENGTH_SHORT);
+    } else {
+      if (response['response_message'] == "Sukses Registrasi") {
+        Fluttertoast.showToast(
+            msg: "Sukses Registrasi", toastLength: Toast.LENGTH_SHORT);
+      } else {
+        Fluttertoast.showToast(
+            msg: "Gagal Registrasi", toastLength: Toast.LENGTH_SHORT);
+      }
+    }
+    setState(() {
+      processing = false;
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+  void userSignIn() async {
+    setState(() {
+      processing = true;
+    });
+    var url = "http://192.168.43.184/skripsi/api/login_api";
+    var data = {
+      "email": emailctrl.text,
+      "password": passctrl.text,
+    };
+
+    var res = await http.post(url, body: data);
+    var response = json.decode(res.body);
+    print(jsonDecode(res.body));
+    if (response['response_message'] == "Belum Punya Akun") {
+      Fluttertoast.showToast(
+          msg: "Anda belum mempunyai akun,silahkan registrasi",
+          toastLength: Toast.LENGTH_SHORT);
+    } else {
+      if (response['response_message'] == "Password Salah") {
+        Fluttertoast.showToast(
+            msg: "Password anda salah", toastLength: Toast.LENGTH_SHORT);
+      } else {
+        Fluttertoast.showToast(
+            msg: "Sukses Login", toastLength: Toast.LENGTH_SHORT);
+      }
+    }
+
+    setState(() {
+      processing = false;
+    });
+  }
+
+  Widget boxUi() {
+    return Card(
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                FlatButton(
+                  onPressed: () => changeState(),
+                  child: Text(
+                    'SIGN IN',
+                    style: GoogleFonts.varelaRound(
+                      color: signin == true ? Colors.blue : Colors.grey,
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () => changeState(),
+                  child: Text(
+                    'SIGN UP',
+                    style: GoogleFonts.varelaRound(
+                      color: signin != true ? Colors.blue : Colors.grey,
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            signin == true ? signInUi() : signUpUi(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget signInUi() {
+    return Column(
+      children: <Widget>[
+        TextField(
+          controller: emailctrl,
+          decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.account_box,
+              ),
+              hintText: 'email'),
+        ),
+        TextField(
+          controller: passctrl,
+          decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.lock,
+              ),
+              hintText: 'pass'),
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        MaterialButton(
+            onPressed: () => userSignIn(),
+            child: processing == false
+                ? Text(
+                    'Sign In',
+                    style: GoogleFonts.varelaRound(
+                        fontSize: 18.0, color: Colors.blue),
+                  )
+                : CircularProgressIndicator(
+                    backgroundColor: Colors.red,
+                  )),
+      ],
+    );
+  }
+
+  Widget signUpUi() {
+    return Column(
+      children: <Widget>[
+        TextField(
+          controller: namectrl,
+          decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.account_box,
+              ),
+              hintText: 'name'),
+        ),
+        TextField(
+          controller: emailctrl,
+          decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.account_box,
+              ),
+              hintText: 'email'),
+        ),
+        TextField(
+          controller: passctrl,
+          decoration: InputDecoration(
+              prefixIcon: Icon(
+                Icons.lock,
+              ),
+              hintText: 'pass'),
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        MaterialButton(
+            onPressed: () => registerUser(),
+            child: processing == false
+                ? Text(
+                    'Sign Up',
+                    style: GoogleFonts.varelaRound(
+                        fontSize: 18.0, color: Colors.blue),
+                  )
+                : CircularProgressIndicator(backgroundColor: Colors.red)),
+      ],
+    );
+  }
+
+  // now we will setup php and database
+//thank you
 }
